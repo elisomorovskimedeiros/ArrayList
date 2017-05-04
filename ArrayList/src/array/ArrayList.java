6c97c18d0227b130a8795613d519158b3c534bb2
MEV <mev@mev.com.br>
2017-05-03 21:50:07 -0300

    Alteração na questão de branch durante apresentação

--- ArrayList/src/array/ArrayList.java
+++ ArrayList/src/array/ArrayList.java
@@ -18,6 +18,7 @@ public class ArrayList {
          
         for (String exe : exe1){
             System.out.println(exe);
+            System.out.println(\n);  //<------- Alteração na questão de branch
         }
     }
      
@@ -25,6 +26,7 @@ public class ArrayList {
         exe1.add("ararara");
         exe1.add("tororo");
         exe1.add("barara");
+        
     }
    //primeira alteração, questão 2
 
